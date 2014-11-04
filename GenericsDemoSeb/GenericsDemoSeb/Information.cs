6b4b679e9763fa15d6f9a867480d38007806f845
TantNordh <tanya . nordh @ gmail .com>
2014-11-04 10:14:24 +0100

    Genericsgenomgång

---
+++ GenericsDemoSeb/GenericsDemoSeb/Information.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsDemoSeb
+{
+	public class Information<T> where T : new()
+	{
+		private T info;
+
+		public Information(T info)
+		{
+			this.info = info;
+		}
+
+		public T GetInfo()
+		{
+			return info;
+		}
+	}
+}
